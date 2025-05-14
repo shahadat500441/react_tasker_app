65ae12b5fef44d00441e2cee4a5baf9dd27c219e
shahadat500441 <smd643925@gmail.com>
2025-05-14 11:39:18 +0600

    project file initialize and header hero and footer section added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Hero from './Hero';
+import Footer from './Footer';
+
+const App = () => {
+  return (
+    <div className="bg-[#191D26] font-[Inter] text-white">
+    <Header></Header>
+    <Hero></Hero>
+    <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
